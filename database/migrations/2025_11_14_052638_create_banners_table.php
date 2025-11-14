d9fb0ca11278e2b18810a2f03fc83b353b6d9245
DevDuulat <duulat.bekm@mail.ru>
2025-11-14 11:51:18 +0600

    Add migration for creating banners table with image path and active status

---
+++ database/migrations/2025_11_14_052638_create_banners_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\BannerStatus;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_path');
+            $table->tinyInteger('is_active')->default(BannerStatus::Active->value); 
+            $table->string('link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
