0077ea58bd6ca3b9dd6c389bb529eccb0d03bb59
DevDuulat <duulat.bekm@mail.ru>
2025-11-18 15:33:36 +0600

    fix(banners): Adjust banner height for larger screens

--- resources/views/sections/banners.blade.php
+++ resources/views/sections/banners.blade.php
@@ -1,5 +1,5 @@
 <div class="container mx-auto px-4 mt-10">
-    <div class="swiper mySwiper relative h-[300px] sm:h-[350px] md:h-[400px] rounded-[30px] shadow-lg z-0">
+    <div class="swiper mySwiper relative h-[300px] sm:h-[350px] md:h-[510px] rounded-[30px] shadow-lg z-0">
         <div class="swiper-wrapper">
             @foreach($banners as $banner)
                 <div class="swiper-slide">
