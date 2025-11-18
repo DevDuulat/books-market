51f040e3b5baaa0ec5ef4df9367441b410fa6b03
DevDuulat <duulat.bekm@mail.ru>
2025-11-18 15:18:59 +0600

    feat(home): Display products and categories on homepage
    Implements product and category display on homepage.
    Fetches banners, categories, and available products with
    category details. Formats product data for view.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,34 @@ namespace App\Http\Controllers;
 
 use App\BannerStatus;
 use App\Models\Banner;
-use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function home(): \Illuminate\View\View
     {
         $banners = Banner::where('is_active', BannerStatus::Active->value)->get();
-        return view('home', compact('banners'));
+        $categories = Category::pluck('name');
+        $products = Product::with('category')
+            ->where('quantity', '>', 0)
+            ->get()
+            ->map(function ($product) {
+                return [
+                    'id' => $product->id,
+                    'name' => $product->name,
+                    'slug' => $product->slug,
+                    'category' => $product->category?->name ?? 'Без категории',
+                    'price' => number_format($product->price, 0, '.', ''),
+                    'old_price' => $product->price + ($product->discount ?? 0),
+                    'img' => $product->image
+                        ? asset('storage/' . $product->image)
+                        : asset('assets/products/no-image.png'),
+                    'quantity' => $product->quantity,
+                ];
+            });
+
+
+        return view('home', compact('categories', 'products', 'banners'));
     }
 }
