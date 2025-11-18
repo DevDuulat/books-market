5d83ee3cf709ca34a2a4daae160d7d28915c79a7
DevDuulat <duulat.bekm@mail.ru>
2025-11-18 15:20:06 +0600

    feat(product): Add product show page
    Adds a new show page for displaying product details, including images, description, and price.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home']);
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('products.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
